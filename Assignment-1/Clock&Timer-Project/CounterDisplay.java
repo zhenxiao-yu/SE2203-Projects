8219ff6b12cdbdbb546c3babc866c27d5b76bf6a
ZhenXiao Yu (Mark) <59629632+zhenxiao-yu@users.noreply.github.com>
2021-04-15 18:40:44 -0400

    Create CounterDisplay.java

---
+++ Assignment-1/Clock&Timer-Project/CounterDisplay.java
@@ -0,0 +1,12 @@
+package Question1;
+
+public interface CounterDisplay {
+    //reset counter to minimum value
+    void reset();
+    //randomize the positions of the wheels of the counter
+    void shuffle();
+    //increment the counter
+    void increase();
+    //decrement the counter
+    void decrease();
+}
